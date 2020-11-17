1754714d083bc6c8be074b75f277c3497437d094
cgonzales <cgonzales@turner-industries.com>
2020-11-16 20:15:41 -0600

    feat: added Header component

--- src/components/CornellNotes.js
+++ src/components/CornellNotes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button, Form} from 'semantic-ui-react';
 
 import { useNotesContext } from '../contexts/NotesContext';
+import Header from './Header';
 import IdeasList from './IdeasList';
 
 const CornellNotes = () => {
@@ -9,6 +10,7 @@ const CornellNotes = () => {
 
   return (
     <Form onSubmit={e => e.preventDefault()}>
+      <Header />
       <IdeasList />
       <Button primary onClick={save}>Save</Button>
     </Form>
